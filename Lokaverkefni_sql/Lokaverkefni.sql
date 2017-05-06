d7d081304c75d0a6e7fb7ddcc4e8de54ef3fa241
Mergjunarhola <jonbe209@nemi.tskoli.is>
2017-05-06 20:17:57 +0000

    table komin upp

--- Lokaverkefni_sql/Lokaverkefni.sql
+++ Lokaverkefni_sql/Lokaverkefni.sql
@@ -24,7 +24,46 @@ CREATE TABLE Flytjandi (
 	Danardagur DATE,
 	Aldur INTEGER NOT NULL,
 	Fjoldi_gefna_diska INTEGER NOT NULL,
-	Flokkur_ID 
-	
-	
+	Flokkur_ID INTEGER,
+	FOREIGN KEY (Flokkur_ID) REFERENCES Flokkur(ID)
 );
+
+CREATE TABLE Diskur (
+	ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+	nafn VARCHAR(50),
+	Utgafudagur DATE NOT NULL,
+	Fjoldi_Laga INTEGER NOT NULL,
+	Tegund_disks_ID INTEGER NOT NULL,
+	Utgefandi_ID INTEGER NOT NULL,
+	FOREIGN KEY (Tegund_disks_ID) REFERENCES Tegund(ID),
+	FOREIGN KEY (Utgefandi_ID) REFERENCES Utgefandi(ID)
+);
+
+CREATE TABLE "Lag" (
+	ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+	nafn VARCHAR(50),
+	Lengd INTEGER NOT NULL,
+	Texti LONGTEXT,
+	Flytjandi_ID INTEGER NOT NULL,
+	Diskur_ID INTEGER NOT NULL,
+	FOREIGN KEY (Flytjandi_ID) REFERENCES Flytjandi(ID),
+	FOREIGN KEY (Diskur_ID) REFERENCES Diskur(ID)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
